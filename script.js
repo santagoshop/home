25cee93b29818d0f5ec846a70dd59b3bd732c07b
santagoshop <104389399+santagoshop@users.noreply.github.com>
2022-05-01 16:30:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -242,7 +242,8 @@ let lang = {
     textRemmers: 'A Remmers 2022-ben hozhatja el számodra a színeket, a védelmet, a minőséget. A Remmers termékekből választhatsz 2022-ben is a körülötted lévő fa kellékek védelmére, hangulatossá tételére. A remmers és a fa, szoros és tartós kapcsolatban áll.',
 
     back: 'Menj vissza a fő oldalra!',
-    textRemmersCard: 'Ezen az oldalon a <b>vizesbázisú, univerzális vékonylazúrt</b> forgalmazzuk kültérre vagy beltérre. Várjuk rendeléseiket erre és az egész Remmers házi termékcsaládra. Ha az alábbi linkre kattintasz a Remmers termékcsaládra kaphatsz rálátást. A fenti link visz a kezdőoldalra.',
+    textRemmersCard: 'Ezen az oldalon a <b>vizesbázisú, univerzális vékonylazúrt</b> forgalmazzuk kültérre vagy beltérre. Várjuk rendeléseiket erre és az egész Remmers házi termékcsaládra. Ha az alábbi linkre kattintasz a Remmers termékcsaládra kaphatsz rálátást. A fenti link visz a kezdőoldalra.
+    Rendelésért az elérhetőség: Ágoston Hajnal, Csíkszereda, +40745983283',
     textRemmersAttention: 'Click ide a részletekért!'
   },
   ro: {
@@ -254,7 +255,8 @@ let lang = {
     textRemmers: 'Remmers și în 2022 poate să-ți aducă culoare, protecție și calitate. Dintre produsele Remmers poți alege și în 2022 pentru a proteja și a da elementelor din lemn din jurul tău culoare. Remmers și lemnul este într-o relație strânsă și durabilă.',
 
     back: 'Mergi pe pagina principală',
-    textRemmersCard: 'Pe această pagină vă oferim Remmers <b>Aidol Universal Holslazur pe bază de apă</b> și așteptăm comenzile dumneavoastră pe toată gama Remmers familia casnică. Dacă dai click mai jos vei vedea familia Remmers și certificatele de calitate. Linkul de mai sus te duce pe prima pagină.',
+    textRemmersCard: 'Pe această pagină vă oferim Remmers <b>Aidol Universal Holslazur pe bază de apă</b> și așteptăm comenzile dumneavoastră pe toată gama Remmers familia casnică. Dacă dai click mai jos vei vedea familia Remmers și certificatele de calitate. Linkul de mai sus te duce pe prima pagină.
+    Pentru comenzi: Ágoston Hajnal, Miercurea Ciuc, +40745983283',
     textRemmersAttention: 'Click aici pentru detalii!'
   }
 }
